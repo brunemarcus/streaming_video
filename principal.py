6b2ce9ffaf3abe1d406416188fc8af33a1d7a78c
Marcus brune <marcusbrune@outlook.com>
2017-10-31 04:12:09 -0200

    principal.py

---
+++ principal.py
@@ -0,0 +1,17 @@
+#filmes
+
+import interface_usuario as ui
+import admnistrador as adm
+import menu_principal as men
+
+def main():
+
+    
+main()
+
+
+
+Alessandre Porta jr 31747027
+Marcus 31747175
+Henrique Silveira 
+
