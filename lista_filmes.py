54766cedd6203664e2c364b7880952b667867ee9
henrysilvers <=>
2017-11-07 01:03:01 -0200

    Lista Filmes

---
+++ lista_filmes.py
@@ -0,0 +1,16 @@
+#menu_filmes
+
+filmes = ['Homem-aranha', 'Vingadores', 'Senhor dos aneis', 'homem de ferro',
+          'Matrix', 'Os caça fantamas', 'Annabelle', 'Invocação do mal',
+          'Animais noturnos' , 'It: A coisa', 'Liga da Justiça', 'Batman Vs Superman' ,
+          'Questão de tempo', 'Jogo Perigoso', '1922','O nome da Rosa', 'O exorcista',
+          'Atividade paranormal', ' O poderoso chefão', 'Eu sou a lenda', 'Contagio',
+          'Noite dos mortos vivos', 'Auto da compadecida', 'Dona flor e seus dois maridos',
+          'Central do Brasil', 'Contato', 'Interestelar', '2001: uma odisseia no espaço',
+          'Capitão Fantastico', 'Fantastica fabrica de chocolate', 'Felicidade não se compra',
+          'Passageiros', 'Rogue one', 'Star wars', 'Star Trek', 'Transformers']
+
+print("Escolha um filme de nosso Catálogo: ")
+for i in enumerate(filmes):
+    print(i)    
+
